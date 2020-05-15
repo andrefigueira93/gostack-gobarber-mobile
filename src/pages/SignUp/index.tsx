1623ce410b4ad5b7ae9fdea37873dd6ff33a379f
andrefigueira93 <andrefigueira93@gmail.com>
2020-05-15 19:11:37 -0300

    Adicionado estilizacao de inputs e validacao dos campos

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
 
 import {
   Image,
@@ -8,8 +11,12 @@ import {
   ScrollView,
   KeyboardAvoidingView,
   Platform,
+  TextInput,
+  Alert,
 } from 'react-native';
 import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
+
+import getValidationErrors from '../../utils/getValidationErrors';
 
 // Components
 import Input from '../../components/Input';
@@ -17,8 +24,55 @@ import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
 const SignUp: React.FC = () => {
   const navigation = useNavigation();
+  const formRef = useRef<FormHandles>(null);
+
+  const emailInputRef = useRef<TextInput>(null);
+  const passwordInputRef = useRef<TextInput>(null);
+
+  const handleSignUp = useCallback(async (data: SignUpFormData) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório!'),
+        email: Yup.string()
+          .required('E-mail obrigatório!')
+          .email('Digite um e-mail válido!'),
+        password: Yup.string().required().min(6, 'Senha mínima de 6 dígitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      // await api.post('/users', data);
+
+      // addToast({
+      //   type: 'success',
+      //   title: 'Cadastro realizado',
+      //   description: 'Você já pode realizar seu logon no GoBarber!',
+      // });
+
+      // history.push('/');
+    } catch (err) {
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors);
+      return;
+    }
+    Alert.alert(
+      'Whoops!',
+      'Ocorreu um erro ao fazer cadastro, tente novamente',
+    );
+  }, []);
+
   return (
     <>
       <KeyboardAvoidingView
@@ -35,16 +89,44 @@ const SignUp: React.FC = () => {
             <View>
               <Title>Crie sua conta</Title>
             </View>
-            <Input name="name" icon="user" placeholder="Nome" />
-            <Input name="email" icon="mail" placeholder="E-mail" />
-            <Input name="password" icon="lock" placeholder="Senha" />
-            <Button
-              onPress={() => {
-                console.log('deu');
-              }}
-            >
-              Cadastrar
-            </Button>
+            <Form ref={formRef} onSubmit={handleSignUp}>
+              <Input
+                name="name"
+                autoCapitalize="words"
+                icon="user"
+                placeholder="Nome"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  emailInputRef.current?.focus();
+                }}
+              />
+              <Input
+                ref={emailInputRef}
+                autoCorrect={false}
+                autoCapitalize="none"
+                keyboardType="email-address"
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+                onSubmitEditing={() => {
+                  passwordInputRef.current?.focus();
+                }}
+                returnKeyType="next"
+              />
+              <Input
+                ref={passwordInputRef}
+                name="password"
+                icon="lock"
+                placeholder="Senha"
+                secureTextEntry
+                textContentType="newPassword"
+                returnKeyType="send"
+                onSubmitEditing={() => formRef.current?.submitForm()}
+              />
+              <Button onPress={() => formRef.current?.submitForm()}>
+                Cadastrar
+              </Button>
+            </Form>
           </Container>
         </ScrollView>
       </KeyboardAvoidingView>
