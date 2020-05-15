06c55569d4b936ee1dd9f1b251211e726743e44e
andrefigueira93 <andrefigueira93@gmail.com>
2020-05-15 14:42:03 -0300

    Added Custom Icons and styled components

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,6 +3,10 @@ import React from 'react';
 import { Image } from 'react-native';
 import { Container, Title } from './styles';
 
+// Components
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
@@ -10,6 +14,15 @@ const SignIn: React.FC = () => {
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu Logon</Title>
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button
+        onPress={() => {
+          console.log('deu');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
